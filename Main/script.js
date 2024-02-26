8eb9d6a94390f7992fd877730aabaf7a96accb10
Atharv_231 <151833161+AtharvCodw@users.noreply.github.com>
2024-02-26 09:28:47 +0530

    Update script.js

--- Main/script.js
+++ Main/script.js
@@ -2,7 +2,7 @@ const generateForm = document.querySelector(".generate-form");
 const generateBtn = generateForm.querySelector(".generate-btn");
 const imageGallery = document.querySelector(".image-gallery");
 
-const OPENAI_API_KEY = "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"; // Your OpenAI API key here
+const OPENAI_API_KEY = "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"; // Your OpenAI API key here
 let isImageGenerating = false;
 
 const updateImageCard = (imgDataArray) => {
